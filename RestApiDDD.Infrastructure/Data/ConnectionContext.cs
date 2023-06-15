cb01a9b32448a35cd9c4d7f77da68c1942a14a68
kayky <kaykyb678@gmail.com>
2023-06-15 08:53:44 -0300

    Add: Configurate DbContext

---
+++ RestApiDDD.Infrastructure/Data/ConnectionContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestApiDDD.Domain.Entities;
+using System;
+using System.Linq;
+
+namespace RestApiDDD.Infrastructure.Data
+{
+    public class ConnectionContext : DbContext
+    {
+        public ConnectionContext() { }
+
+        public ConnectionContext(DbContextOptions<ConnectionContext> options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("RegistrationDate") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("RegistrationDate").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("RegistrationDate").IsModified = false;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}

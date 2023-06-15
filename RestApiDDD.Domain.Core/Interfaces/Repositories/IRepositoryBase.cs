b4ba0729d6edc972eebbf47079299ece87deb3bc
kayky <kaykyb678@gmail.com>
2023-06-15 09:14:46 -0300

    Add: Entities Repositories

---
+++ RestApiDDD.Domain.Core/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace RestApiDDD.Domain.Core.Interfaces.Repositories
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+    }
+}
---
+++ RestApiDDD.Domain.Core/Interfaces/Repositories/IRepositoryClient.cs
@@ -0,0 +1,8 @@
+﻿using RestApiDDD.Domain.Entities;
+
+namespace RestApiDDD.Domain.Core.Interfaces.Repositories
+{
+    public interface IRepositoryClient : IRepositoryBase<Client>
+    {
+    }
+}

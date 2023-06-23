b7e8f163dd6daeec484ab87ba01193831195d15d
kayky <kaykyb678@gmail.com>
2023-06-23 16:33:29 -0300

    Alter: Configurations in the string length

--- RestApiDDD.Application/DTOs/Request/ClientRequestDTO.cs
+++ RestApiDDD.Application/DTOs/Request/ClientRequestDTO.cs
@@ -5,15 +5,15 @@ namespace RestApiDDD.Application.DTOs.Request
     public class ClientRequestDTO
     {
         [Required(ErrorMessage = "Post FirstName is required.")]
-        [StringLength(100, MinimumLength = 5)]
+        [StringLength(50, MinimumLength = 5)]
         public string? FirstName { get; set; }
         
         [Required(ErrorMessage = "Post LastName is required.")]
-        [StringLength(100, MinimumLength = 5)]
+        [StringLength(50, MinimumLength = 5)]
         public string? LastName { get; set; }
 
         [Required(ErrorMessage = "Post Email is required.")]
-        [StringLength(100, MinimumLength = 10)]
+        [StringLength(30, MinimumLength = 12)]
         public string? Email { get; set; }
     }
 }

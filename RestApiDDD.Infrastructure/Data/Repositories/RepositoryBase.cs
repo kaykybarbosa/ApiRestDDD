3b41fb0aabbc2a1b002ce09362d86a2478a28d88
kayky <kaykyb678@gmail.com>
2023-06-23 16:39:13 -0300

    Alter: Types return

--- RestApiDDD.Infrastructure/Data/Repositories/RepositoryBase.cs
+++ RestApiDDD.Infrastructure/Data/Repositories/RepositoryBase.cs
@@ -9,26 +9,30 @@ namespace RestApiDDD.Infrastructure.Data.Repositories
 
         public RepositoryBase(ConnectionContext context) => _context = context;
 
-        public void Add(TEntity entity)
+        public async Task<TEntity> Add(TEntity entity)
         {
-            _context.Set<TEntity>().Add(entity);
-            _context.SaveChanges();
+           _context.Set<TEntity>().Add(entity);
+           _context.SaveChanges();
+            
+           return entity;
         }
-
-        public void Delete(TEntity entity)
+        public async Task<TEntity> Delete(TEntity entity)
         {
            _context.Set<TEntity>().Remove(entity);
            _context.SaveChanges();
-        }
 
+            return entity;
+        }
         public IEnumerable<TEntity> GetAll() => _context.Set<TEntity>().ToList();
 
-        public TEntity GetById(Guid id) => _context.Set<TEntity>().Find(id);
+        public async Task<TEntity?> GetById(Guid id) => _context.Set<TEntity>().Find(id);
 
-        public void Update(TEntity entity)
+        public async Task<TEntity> Update(TEntity entity)
         {
             _context.Entry(entity).State = EntityState.Modified;
             _context.SaveChanges();
+
+            return entity;
         }
     }
 }

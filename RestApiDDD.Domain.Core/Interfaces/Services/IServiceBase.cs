1690d032824bdad4adca7151b2aa16fd47b0fbd6
kayky <kaykyb678@gmail.com>
2023-06-23 16:38:34 -0300

    Alter: Types return

--- RestApiDDD.Domain.Core/Interfaces/Services/IServiceBase.cs
+++ RestApiDDD.Domain.Core/Interfaces/Services/IServiceBase.cs
@@ -2,10 +2,10 @@
 {
     public interface IServiceBase<TEntity> where TEntity : class
     {
-        void Add(TEntity entity);
-        void Update(TEntity entity);
-        void Delete(TEntity entity);
+        Task<TEntity> Add(TEntity entity);
+        Task<TEntity> Update(TEntity entity);
+        Task<TEntity> Delete(TEntity entity);
         IEnumerable<TEntity> GetAll();  
-        TEntity GetById(Guid id);
+        Task<TEntity> GetById(Guid id);
     }
 }

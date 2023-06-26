2e034a637c3e109fa94e009a65ea6f66219cc3b7
kayky <kaykyb678@gmail.com>
2023-06-26 10:22:23 -0300

    Alter: improving Dtos

--- RestApiDDD.Application/DTOs/Response/BaseResponseDTO.cs
+++ RestApiDDD.Application/DTOs/Response/BaseResponseDTO.cs
@@ -1,12 +1,22 @@
-﻿namespace RestApiDDD.Application.DTOs.Response
+﻿using System.Text.Json.Serialization;
+
+namespace RestApiDDD.Application.DTOs.Response
 {
     public class BaseResponseDTO
     {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Message { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
         public bool Success { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Error { get; set; }
 
         public BaseResponseDTO() { }
+        public BaseResponseDTO(string? message, bool success)
+        {
+            Message = message; 
+            Success = success; 
+        }
         public BaseResponseDTO(string? message, bool success, string? error)
         {
             Message = message; 
--- RestApiDDD.Application/DTOs/Response/ProductResponseDTO.cs
+++ RestApiDDD.Application/DTOs/Response/ProductResponseDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace RestApiDDD.Application.DTOs.Response
+﻿using System.Text.Json.Serialization;
+
+namespace RestApiDDD.Application.DTOs.Response
 {
-    public class ProductResponseDTO
+    public class ProductResponseDTO : BaseResponseDTO
     {
-        public Guid Guid { get; set; }
+        public Guid Id { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public string? Name { get; set; }
         public decimal Price { get; set; }
         public bool IsAvaiable { get; set; }

fe38bf80e9b9856860110ecf33b1a3bf1d5bfc26
kayky <kaykyb678@gmail.com>
2023-07-10 14:10:34 -0300

    Alter: adding mistakes unit test

--- RestaApi.Api.Tests/Services/ApplicationServiceClientTest.cs
+++ RestaApi.Api.Tests/Services/ApplicationServiceClientTest.cs
@@ -1,7 +1,5 @@
-﻿using Azure.Core;
-using FakeItEasy;
+﻿using FakeItEasy;
 using FluentAssertions;
-using Microsoft.Identity.Client;
 using RestApiDDD.Application;
 using RestApiDDD.Application.DTOs.Request;
 using RestApiDDD.Application.DTOs.Request.Update;
@@ -29,9 +27,8 @@ namespace RestaApi.Api.Tests.Services
         public async Task ApplicationServiceClient_Add_ReturnSuccess()
         {
             //Arrange
+            Client? nullClient = null;
             var client = A.Fake<Client>();
-            var value = A.Fake<Task<Client>>();
-            value = null;
 
             ClientRequestDTO request = new() 
             {
@@ -47,7 +44,7 @@ namespace RestaApi.Api.Tests.Services
             };
 
             A.CallTo(() => _mapperClient.MapperDtoToEntity(request)).Returns(new Client());
-            A.CallTo(() => _serviceClient.GetByEmail(A<String>.Ignored)).Returns(value);
+            A.CallTo(() => _serviceClient.GetByEmail(A<String>.Ignored)).Returns(nullClient);
             A.CallTo(() => _serviceClient.Add(client)).Returns(client);
 
             //Act
@@ -56,6 +53,36 @@ namespace RestaApi.Api.Tests.Services
             //Assert
             result.Should().BeEquivalentTo(response);
             result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task ApplicationServiceClient_Add_ReturnError()
+        {
+            //Arrange
+            var client = A.Fake<Client>();
+
+            ClientRequestDTO request = new() 
+            { 
+                FirstName = client.FirstName,
+                LastName = client.LastName,
+                Email = client.Email
+            };
+
+            BaseResponseDTO response = new()
+            {
+                Message = "Client with this Email already exists.",
+                Success = false
+            };
+
+            A.CallTo(() => _mapperClient.MapperDtoToEntity(request)).Returns(client);
+            A.CallTo(() => _serviceClient.Add(client)).Returns(Task.FromResult(client));
+
+            //Act
+            var result = await _applicationClient.Add(request);
+
+            //Assert
+            result.Should().BeEquivalentTo(response);
+            result.Success.Should().BeFalse();
         }
 
         [Fact]
@@ -85,6 +112,29 @@ namespace RestaApi.Api.Tests.Services
             //Assert
             result.Should().BeEquivalentTo(response);
             result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task ApplicationServiceClient_GetById_ReturnError()
+        {
+            //Arrange
+            Client? nullClient = null;
+            var idInexistent = Guid.NewGuid();
+
+            ClientResponseDTO response = new()
+            {
+                Message = "Client with this Id not found.",
+                Success = false
+            };
+
+            A.CallTo(() => _serviceClient.GetById(idInexistent)).Returns(nullClient);
+
+            //Act
+            var result = await _applicationClient.GetById(idInexistent);
+
+            //Assert
+            result.Should().BeEquivalentTo(response);
+            result.Success.Should().BeFalse();
         }
 
         [Fact]
@@ -129,6 +179,29 @@ namespace RestaApi.Api.Tests.Services
         }
 
         [Fact]
+        public async Task ApplicationServiceClient_Delete_ReturnError()
+        {
+            //Arrange
+            var idInexistent = Guid.NewGuid();
+            Client? nullClient = null;
+
+            BaseResponseDTO response = new()
+            {
+                Message = "Client with this Id not found.",
+                Success = false
+            };
+
+            A.CallTo(() => _serviceClient.GetById(idInexistent)).Returns(nullClient);
+
+            //Act
+            var result = await _applicationClient.Delete(idInexistent);
+
+            //Assert
+            result.Should().BeEquivalentTo(response);
+            result.Success.Should().BeFalse();
+        }
+ 
+        [Fact]
         public async Task ApplicationServiceClient_Update_ReturnSuccess()
         {
             //Arrange
@@ -157,6 +230,37 @@ namespace RestaApi.Api.Tests.Services
             //Assert
             result.Should().BeEquivalentTo(response);
             result.Success.Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task ApplicationServiceClient_Update_ReturnError()
+        {
+            //Arrange
+            var idInexistent = Guid.NewGuid();
+            Client? nullClient = null;
+
+            ClientRequestUpdateDTO request = new()
+            {
+                FirstName = "Test",
+                LastName = "Test",
+                Email = "Test",
+                IsActive = false
+            };
+
+            BaseResponseDTO response = new()
+            {
+                Message = "Client with this Id not found.",
+                Success = false
+            };
+
+            A.CallTo(() => _serviceClient.GetById(idInexistent)).Returns(nullClient);
+
+            //Act
+            var result = await _applicationClient.Update(idInexistent, request);
+
+            //Assert
+            result.Should().BeEquivalentTo(response);
+            result.Success.Should().BeFalse();
         }
     }
 }
